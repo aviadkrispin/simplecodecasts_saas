e083d18cd5a1316138d962d9934f0783d1fcb903
Aviad Krispin <aviad.krispin@gmail.com>
2016-04-28 21:20:30 +0000

    fixed SENDGRID_USERNAME

--- app/controllers/concerns/contacts_controller.rb
+++ app/controllers/concerns/contacts_controller.rb
@@ -18,7 +18,7 @@ class ContactsController < ApplicationController
         else 
             flash[:warning]="Error occured, message has not been sent"
          redirect_to new_contact_path 
-     end
+        end
     end
     
  private 
--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
  :address => "smtp.sendgrid.net",
  :port =>"587",
  :authentication => :plain,
- :user_name => ENV["SENGRID_USERNAME"],
- :password => ENV["SENGRRID_PASSWORD"],
+ :user_name => ENV["SENDGRID_USERNAME"],
+ :password => ENV["SENDGRRID_PASSWORD"],
  :domain =>"heroku.com",
  :enable_startstls_auto => true
 }
